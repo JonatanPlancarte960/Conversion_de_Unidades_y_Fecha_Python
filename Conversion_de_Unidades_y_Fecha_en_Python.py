3043f38a129d360e9a02ef064e93c948cb39af21
Jonatan Plancarte <jonatanplancarte960is@gmail.com>
2023-03-15 19:45:26 -0600

    Fecha y Unidades Python

---
+++ Conversion_de_Unidades_y_Fecha_en_Python.py
@@ -0,0 +1,14 @@
+#Fecha
+
+from datetime import date
+fecha = date.today() # int
+print(fecha)
+
+#Años Luz
+
+parsecs = 11
+Lightyears = parsecs * 3.26
+print("11 Parsecs son " + str(Lightyears) + " Lightyears")
+
+
+
---
+++ README.md
@@ -0,0 +1,3 @@
+# Conversion de Unidades y Uso de Fecha en Python
+
+## Conversion de Unidades parsecs - años luz y Fecha con el Comando datetime
